2d7ba3e294aa0b783eab30363387aac70e9fef38
Anthony Tarr <me@anthonytarr.net>
2026-06-26 11:34:24 -0700

    Add tests

--- src/utils.rs
+++ src/utils.rs
@@ -32,3 +32,35 @@ pub fn parse_clip_string(clip_string: &str) -> (u64, u64, u64) {
 
     (clip_number, date, time)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::path::PathBuf;
+
+    #[test]
+    fn test_sort_chunks() {
+        let mut chunks = vec![
+            PathBuf::from("chunk-stream0-2"),
+            PathBuf::from("chunk-stream0-0"),
+            PathBuf::from("chunk-stream0-1"),
+        ];
+        sort_chunks(&mut chunks);
+        assert_eq!(
+            chunks,
+            vec![
+                PathBuf::from("chunk-stream0-0"),
+                PathBuf::from("chunk-stream0-1"),
+                PathBuf::from("chunk-stream0-2"),
+            ]
+        );
+    }
+
+    #[test]
+    fn test_parse_clip_string() {
+        let (steam_id, date, time) = parse_clip_string("clip_12345_20250101_120000");
+        assert_eq!(steam_id, 12345);
+        assert_eq!(date, 20250101);
+        assert_eq!(time, 120000);
+    }
+}
